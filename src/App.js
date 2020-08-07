8d33251fd7e4bbadfce9681b2521bc5735b8b385
Luis Garcia <luisg5@uci.edu>
2020-08-06 22:19:49 -0700

    add movie sections slider.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Content from "./components/Content"
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
