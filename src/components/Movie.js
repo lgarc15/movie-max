8d92707b324a750678c91eceeaf62d83bbac7854
Luis Garcia <luisg5@uci.edu>
2020-08-09 21:17:25 -0700

    seperate movie sections to component.

---
+++ src/components/Movie.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../App.css"
+import "../stylesheets/Movie.css"
+
+
+export default class TopNavbar extends React.Component {
+  render() {
+    return (
+      <div className="" id="movie">
+          Movie
+      </div>
+    );
+  }
+}
