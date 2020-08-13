1d6499bc0696114c44c8ccd2684a1fdf354c4736
Luis Garcia <luisg5@uci.edu>
2020-08-13 16:00:40 -0700

    make navbar brand clickable.

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-import brandLogo from "../images/brand_logo.png"
+import brandLogo from "../images/brand_logo.png";
 
 import "../App.css";
 import "../stylesheets/TopNavbar.css";
@@ -11,14 +12,19 @@ export default class TopNavbar extends React.Component {
     return (
       <Navbar expand="lg" id="top-navbar">
         <Navbar.Brand className="my-cl-tertiary" id="top-navbar-brand">
-          <img
-            alt=""
-            src={brandLogo}
-            width="60"
-            height="60"
-            className="d-inline-block align-top"
-          />{" "}
-          MovieMax
+          <Link to={{
+              pathname: "/",
+              hash: "#main-container"
+            }}>
+            <img
+              alt=""
+              src={brandLogo}
+              width="60"
+              height="60"
+              className="d-inline-block align-top"
+            />{" "}
+            MovieMax
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="top-navbar-content" />
         <Navbar.Collapse id="top-navbar-content">
